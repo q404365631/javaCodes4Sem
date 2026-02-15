78d852e3e5b6d42c5008a5e0cdc6fcba021324b2
Rishabh Paudel <24BCS10110@cuchd.in>
2026-02-15 21:56:34 +0530

    added  comments in abst2

--- 06-02-2026/abst2.java
+++ 06-02-2026/abst2.java
@@ -1,17 +1,19 @@
 
 abstract class abst {
-    int x =1;
+    int x = 1;
     abstract void show();
     void display() {System.err.println("Abstraction display function");}
 }
 class xyz extends abst {
     void show() {
         System.err.println("Abstract Show function");
-        System.out.println("x = " +super.x);
-        super.display();
+        System.out.println("x = " +super.x);            // yup "super" which accesses parent member functions
+        super.display();                                // calling display() method of abst class usin super
     }
     public static void main(String[] args) {
         xyz x = new xyz();
         x.show();
     }
 }
+
+// this is similar to abst1 ryt? just extra methods and all
