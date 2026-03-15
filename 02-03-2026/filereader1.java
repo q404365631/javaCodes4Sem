0071a9ee7163fca63f708e1e2cabf1d54e04dad4
Rishabh Paudel <24BCS10110@cuchd.in>
2026-03-15 20:53:18 +0530

    fixed all error comin in filereader1,2 and 3 yayaya

--- 02-03-2026/filereader1.java
+++ 02-03-2026/filereader1.java
@@ -5,6 +5,8 @@ class filereader1 {
         try {
             FileReader fr = new FileReader("javaCodes4Sem\\02-03-2026\\test.txt"); // there are 2 types of paths one is absolute and other is relative in this i have used relative path
             System.out.println(fr.read()); // output will be 69 as ASKII value of E is 69
+
+            fr.close();
         }
         catch (Exception e) {
             System.err.println(e);
--- 02-03-2026/filereader3.java
+++ 02-03-2026/filereader3.java
@@ -5,9 +5,11 @@ class filereader2 {
         try {
             FileReader fr = new FileReader("javaCodes4Sem\\02-03-2026\\test.txt");
             int i;
-            // tere is sometin missing in below condition
-            while (i  = fr.read() != -1) {  // this states that this while loop should work until every chareceter is not read and if char are done it will be -1
+            
+            while ((i  = fr.read()) != -1) {  // this states that this while loop should work until every chareceter is not read and if char are done it will be -1
                 System.out.print((char)i);    // it will only print whole file all text as it is.
+
+                fr.close();
             }
         }
         catch(Exception e) {
