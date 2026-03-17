5158437b627a25006c6873941f0e4be8dfb6da8e
Rishabh Paudel <24BCS10110@cuchd.in>
2026-03-17 22:59:35 +0530

    added thread4.java

---
+++ 16-03-2026/thread4.java
@@ -0,0 +1,20 @@
+class xyz extends Thread {
+    public void run() {
+        for(int i = 0; i<=3; i++) {
+            try {
+                Thread.sleep(10000);
+            }
+            catch(Exception e) { }
+
+            System.out.println(i);
+            System.out.println("Thread Name: " +Thread.currentThread().getName());      // sometin new
+        }
+    }
+    public static void main(String[] args) {
+        xyz x1, x2;
+        x1 = new xyz();
+        x2 = new xyz();
+        x1.start();
+        x2.start();
+    }
+}
