33d5486278b2d30792c300d7c587c08b98f9e224
Rishabh Paudel <24BCS10110@cuchd.in>
2026-03-19 09:21:15 +0530

    added importinPAckage file java

--- 10-03-1016/importinPackage.java
+++ 10-03-1016/importinPackage.java
@@ -1,1 +1,12 @@
-// importin packaje xyz code
+import xyz.*;
+
+class mno {
+    public static void main(String[] args) {
+        ABC abc = new ABC();
+        // mno m = new mno();
+
+        abc.get();
+        abc.show();
+        // m.get();
+    }
+}
