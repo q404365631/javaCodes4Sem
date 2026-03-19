ef97e931977e83b760b9bde962ee3c87296ba394
Rishabh Paudel <24BCS10110@cuchd.in>
2026-03-19 09:14:50 +0530

    created package file

--- 10-03-1016/xyz/ABC.java
+++ 10-03-1016/xyz/ABC.java
@@ -1,1 +1,10 @@
-// package folder
+package xyz;
+
+public class ABC {
+    public void get() {
+        System.out.print("Hello from package's get method");
+    }
+    public void show() {
+        System.out.print("Hello from package's show method");
+    }
+}
