0036e8f7a07f02821ed8bd60fdd432a61fcc8854
Rishabh Paudel <24BCS10110@cuchd.in>
2026-03-25 22:46:08 +0530

    edited awd 1

--- 24-03-2026/awd1.java
+++ 24-03-2026/awd1.java
@@ -2,6 +2,11 @@ import java.awd.*;
 
 class xyz extends Frame {
     xyz() {             // constructor
-        // code
+        SetSize(100,100);
+        SetVisible(true);   // default false
+        SetTitle("Hello");
+    }
+    public static void main(String[] args) {
+        xyz x = new xyz();
     }
 }
