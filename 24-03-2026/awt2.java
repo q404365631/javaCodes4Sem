dc58ac8e88c47d811f1f903fdcafa8093302ba86
Rishabh Paudel <24BCS10110@cuchd.in>
2026-03-27 23:34:38 +0530

    added awt2.java

--- 24-03-2026/awt2.java
+++ 24-03-2026/awt2.java
@@ -4,6 +4,11 @@ import java.awt.*;
 
 class xyz {
     public static void main(String[] args) {
-        Frame f = new Frame("First Frame")
+        Frame f = new Frame("First Frame");
+
+        f.setSize(100,100);
+        f.setVisible(true);         // default false
+        f.setTitle("Registration"); // the text wich is written on tab with internet globe icon
+        
     }
 }
