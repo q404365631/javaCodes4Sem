9f19d7840193f92827964b42e5c6c9b35523ca5b
Rishabh Paudel <24BCS10110@cuchd.in>
2026-04-07 22:45:19 +0530

    EDIted 31-3-2026

--- 30-03-2026/awt1.java
+++ 30-03-2026/awt1.java
@@ -2,6 +2,12 @@ import java.awt.*;
 
 class test {
     public static void main(String args) {
+        Frame f= new Frame("test");
+        Label l1 = new Label("Username/Email");
+        l1.setBounds(40, 50, 120, 40);
+        l1.setBackground(Color.RED);
+        TextField user = new TextField();
+
         // code
     }
 }
