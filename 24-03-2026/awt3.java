92a93127f4e19c1d97b97f8a602b573df8876fbc
Rishabh Paudel <24BCS10110@cuchd.in>
2026-04-07 22:51:38 +0530

    EDIted 24-3-2026

---
+++ 24-03-2026/awt3.java
@@ -0,0 +1,16 @@
+// import java.awt.event.ActionEvent;
+// import java.awt.event.ActionListener;
+
+// class Listen implements ActionListener {
+//     public void ActionPerformed(ActionEvent e) {
+//         System.out.print("Hello");
+//     }
+// }
+// .
+// .
+// .
+
+// Listen l = new Listen();
+// b.addActionListener(L);
+
+// hmm noic
