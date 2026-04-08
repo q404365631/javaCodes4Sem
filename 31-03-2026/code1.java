4d5ce996d358720fda2e40fc3f7cc6fc1caf0108
Rishabh Paudel <24BCS10110@cuchd.in>
2026-04-08 23:42:00 +0530

    added more lines in 31's code

--- 31-03-2026/code1.java
+++ 31-03-2026/code1.java
@@ -1,10 +1,16 @@
 import java.awt.*;
+import java.awt.event.WindowAdapter;
 
 
 class xyz {
     public static void main(String[] args) {
         Frame f = new Frame("JJ");
         f.setSize(400,400);
+        f.setVisible(true);
+
+        f.addWindowListener(new WindowAdapter() {   // WindowAdapter() is new tin
+            // some un known code
+        });
 
         // some code
     }
