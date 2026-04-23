f0b47421fa87905c401790eab70d8384ae1813fb
Rishabh Paudel <24BCS10110@cuchd.in>
2026-04-23 22:43:23 +0530

    added code3 but seems like i will add an issue

---
+++ 06-04-2026/code3.java
@@ -0,0 +1,12 @@
+import java.applet.*;
+import java.awt.*; 
+
+class xyz extends Applet {
+    Button b1;
+
+    setLayout(new BorderLayout());
+    add(b1.BorderLayout.NORTH);
+    add(L1.BorderLayout.WEST);
+}
+
+// idk whats happenin
